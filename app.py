5ab84ef8e96f42173121aa40f05bc71f955f79cb
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-21 14:34:54 +0600

    using template and dynamic content

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    recent_post = 'This is recent post from blog'
+    return render_template('index.html', recent_post=recent_post)
 
 @app.route('/me')
 def profile():
@@ -16,4 +17,4 @@ def blog():
 
 @app.route('/blog/<int:id>')
 def article(id):
-    return f"Post {id}".upper()
+    return f"Post {id}"
