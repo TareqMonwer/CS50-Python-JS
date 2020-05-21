11803ac08f4a6afe697dfa4493af80df1d63ab95
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-21 14:14:21 +0600

    adding more basic routes

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello world!"
+
+@app.route('/me')
+def profile():
+    return "Tareq Monwer"
+
+@app.route('/blog')
+def blog():
+    return "My personal blog"
+
+@app.route('/blog/<int:id>')
+def article(id):
+    return f"Post {id}".upper()
