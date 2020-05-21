ba8aced7bf86db009fc9f0161d2effc04cc2679f
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-21 16:38:54 +0600

    storing data with session

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request
+from flask_session import Session
 
+
+## App configs
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+# Global data with session
+categories = ['Learning English', 'Python', 'Django']
+
+## Rotes
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    categories = ['Learning English', 'Python', 'Django']
+    if request.method == 'POST':
+        category = request.form.get('category')
+        categories.append(category)
+
     recent_post = 'This is recent post from blog'
     return render_template('index.html',
         recent_post=recent_post, categories=categories)
@@ -18,7 +32,6 @@ def about():
         mail = request.form.get('email')
         text = request.form.get('message')
         return f'{name} {mail} {text}'
-    categories = ['Learning English', 'Python', 'Django']
     return render_template('me.html', categories=categories)
 
 
