b7aa5a2131625711c0e4aa933076ebdd36d52850
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-21 15:28:54 +0600

    url dispathing and template inheritance

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    categories = ['Learning English', 'Python', 'Django']
     recent_post = 'This is recent post from blog'
-    return render_template('index.html', recent_post=recent_post)
+    return render_template('index.html',
+        recent_post=recent_post, categories=categories)
 
 @app.route('/me')
-def profile():
-    return "Tareq Monwer"
+def about():
+    categories = ['Learning English', 'Python', 'Django']
+    return render_template('me.html', categories=categories)
 
 @app.route('/blog')
 def blog():
