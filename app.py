f499c8495a2be9fb333b1567e46adc5004638c56
TareqMonwer <tareqmonwer137@gmail.com>
2020-05-21 16:14:32 +0600

    form and request methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,14 +9,23 @@ def index():
     return render_template('index.html',
         recent_post=recent_post, categories=categories)
 
-@app.route('/me')
+
+@app.route('/me', methods=['GET','POST'])
 def about():
+    # Form data
+    if request.method == 'POST':
+        name = request.form.get('name')
+        mail = request.form.get('email')
+        text = request.form.get('message')
+        return f'{name} {mail} {text}'
     categories = ['Learning English', 'Python', 'Django']
     return render_template('me.html', categories=categories)
+
 
 @app.route('/blog')
 def blog():
     return "My personal blog"
+
 
 @app.route('/blog/<int:id>')
 def article(id):
